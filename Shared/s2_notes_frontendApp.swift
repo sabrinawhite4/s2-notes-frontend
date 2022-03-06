e4e6f24c1d76d6b686fca2b5f513ecc488d72c52
Sabrina White <sabrinawhite4@me.com>
2022-03-05 22:31:22 -0700

    Initial Commit

---
+++ Shared/s2_notes_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  s2_notes_frontendApp.swift
+//  Shared
+//
+//  Created by Sabrina White on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct s2_notes_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
